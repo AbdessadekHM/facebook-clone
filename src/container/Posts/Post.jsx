6fb1e94d18d6af9a2d46751fca0cd2b72b6ba8f6
Abdessadek Hmiddouch <63494437+AbdessadekHM@users.noreply.github.com>
2024-02-14 23:36:34 +0100

    Update Post.jsx
    Add bug in "src/Posts/Post.jsx"

--- src/container/Posts/Post.jsx
+++ src/container/Posts/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { getDocs, collection, query, where } from 'firebase/firestore';
 
 import { db } from '../../config/firebase';
